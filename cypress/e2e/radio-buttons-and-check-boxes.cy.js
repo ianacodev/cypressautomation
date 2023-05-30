c0e2d4355b24a85e4764b24cd48081f290263c9e
ianacodev <ianaco.dev@gmail.com>
2023-05-30 10:59:35 -0400

    update

--- cypress/e2e/radio-buttons-and-check-boxes.cy.js
+++ cypress/e2e/radio-buttons-and-check-boxes.cy.js
@@ -1,1 +1,24 @@
-describe('', () => {});
+describe('check ui elements', () => {
+  beforeEach(() => {
+    cy.visit('https://itera-qa.azurewebsites.net/home/automation');
+  });
+
+  it('checking radio buttons', () => {
+    // count
+    cy.get(':nth-child(4) > .card-body input[type=radio]').should(
+      'have.length',
+      '3',
+    );
+
+    cy.get('input#male').should('be.visible');
+    cy.get('input#female').should('be.visible');
+
+    // check male
+    cy.get('input#male').check().should('be.checked');
+    cy.get('input#female').should('not.be.checked');
+
+    // female
+    cy.get('input#female').check().should('be.checked');
+    cy.get('input#male').should('not.be.checked');
+  });
+});
